eecaca63b757fac1a7a6403639e5b855754a26e5
Henrique Araujo <henriquearaujodev@gmail.com>
2022-11-13 21:56:03 -0300

    fix resposive menu mobile

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,9 +1,11 @@
 const btn = document.querySelector('.open-close');
 const menuMobile = document.querySelector('.menu-mobile');
-const body = document.querySelector('body');
+const overlay = document.querySelector('.overlay');
 
 btn.addEventListener('click', () => {
   let open = menuMobile.classList.contains('menu-mobile-open');
+
+  overlay.classList.toggle('overlay-open');
 
   !open
     ? menuMobile.classList.add('menu-mobile-open')
