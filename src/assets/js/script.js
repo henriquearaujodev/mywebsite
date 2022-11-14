7e516b8a12964951ccb78ce385728ba948f7ac64
Henrique Araujo <henriquearaujodev@gmail.com>
2022-11-13 21:39:40 -0300

    fix resposive mediaqueries

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -3,12 +3,9 @@ const menuMobile = document.querySelector('.menu-mobile');
 const body = document.querySelector('body');
 
 btn.addEventListener('click', () => {
-  console.log('foiie');
-
   let open = menuMobile.classList.contains('menu-mobile-open');
-  if (!open) {
-    menuMobile.classList.add('menu-mobile-open');
-  } else {
-    menuMobile.classList.remove('menu-mobile-open');
-  }
+
+  !open
+    ? menuMobile.classList.add('menu-mobile-open')
+    : menuMobile.classList.remove('menu-mobile-open');
 });
