209fd6690a2850a5a8d0e78e950e0320fdc44925
Henrique Araujo <henriquearaujodev@gmail.com>
2022-11-13 22:18:32 -0300

    fix icon menu mobile

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,15 +1,20 @@
 const btn = document.querySelector('.open-close');
 const menuMobile = document.querySelector('.menu-mobile');
 const overlay = document.querySelector('.overlay');
+const openclose = document.querySelector('.open-close');
 
 btn.addEventListener('click', () => {
   let open = menuMobile.classList.contains('menu-mobile-open');
 
   overlay.classList.toggle('overlay-open');
 
-  !open
-    ? menuMobile.classList.add('menu-mobile-open')
-    : menuMobile.classList.remove('menu-mobile-open');
+  if (!open) {
+    menuMobile.classList.add('menu-mobile-open');
+    openclose.src = './assets/img/close.svg';
+  } else {
+    menuMobile.classList.remove('menu-mobile-open');
+    openclose.src = './assets/img/menu.svg';
+  }
 });
 
 overlay.addEventListener('click', () => {
