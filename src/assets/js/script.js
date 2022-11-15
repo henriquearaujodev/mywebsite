f4a1a1e648fbe7093c9d51f83e33564f2a9cbb9f
Henrique Araujo <henriquearaujodev@gmail.com>
2022-11-15 11:50:05 -0300

    add modal full screen

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,20 +1,22 @@
-const btn = document.querySelector('.open-close');
+const btnOpen = document.querySelector('.btn-open');
+const btnClose = document.querySelector('.btn-close');
 const menuMobile = document.querySelector('.menu-mobile');
 const overlay = document.querySelector('.overlay');
 const openclose = document.querySelector('.open-close');
 
-btn.addEventListener('click', () => {
+btnOpen.addEventListener('click', () => {
   let open = menuMobile.classList.contains('menu-mobile-open');
-
-  overlay.classList.toggle('overlay-open');
 
   if (!open) {
     menuMobile.classList.add('menu-mobile-open');
-    openclose.src = './assets/img/close.svg';
   } else {
     menuMobile.classList.remove('menu-mobile-open');
-    openclose.src = './assets/img/menu.svg';
   }
+});
+
+btnClose.addEventListener('click', () => {
+  menuMobile.classList.remove('menu-mobile-open');
+  console.log('fechou');
 });
 
 overlay.addEventListener('click', () => {
